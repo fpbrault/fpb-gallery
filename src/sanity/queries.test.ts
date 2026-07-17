082c7160fd90ef7e615e91dd34976a2fe0e9fb2f
Felix Perron-Brault <fpbrault@gmail.com>
2026-07-17 11:14:51 -0400

    fix: finish internationalized array v5 migration

--- src/sanity/queries.test.ts
+++ src/sanity/queries.test.ts
@@ -7,11 +7,12 @@ import {
   POST_QUERY
 } from "./queries";
 
-const COMPATIBLE_SELECTOR = "language == $locale || _key == $locale";
+const V5_SELECTOR = "language == $locale";
 
 describe("internationalized array queries", () => {
-  it("supports v4 and v5 album descriptions", () => {
-    expect(ALBUM_QUERY).toContain(`albumContent[${COMPATIBLE_SELECTOR}][0].value`);
+  it("selects v5 album descriptions by language", () => {
+    expect(ALBUM_QUERY).toContain(`albumContent[${V5_SELECTOR}][0].value`);
+    expect(ALBUM_QUERY).not.toContain("_key == $locale");
   });
 
   it.each([
@@ -19,8 +20,8 @@ describe("internationalized array queries", () => {
     ["post cursor", POST_CURSOR_QUERY, 2],
     ["latest post", LATEST_POST_QUERY, 2],
     ["post detail and navigation", POST_QUERY, 6]
-  ])("supports v4 and v5 localized values in the %s query", (_name, query, expectedCount) => {
-    expect(query.split(COMPATIBLE_SELECTOR)).toHaveLength(expectedCount + 1);
-    expect(query).not.toMatch(/\[(?:title|postContent|albumContent)?_key == \$locale\]/);
+  ])("selects v5 localized values in the %s query", (_name, query, expectedCount) => {
+    expect(query.split(V5_SELECTOR)).toHaveLength(expectedCount + 1);
+    expect(query).not.toContain("_key == $locale");
   });
 });
